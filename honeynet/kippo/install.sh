9e55ae85140638bef646e2696e01468b0a58001b
Spenser Reinhardt <spenserreinhardt@gmail.com>
2014-09-11 20:56:02 -0500

    kippo, conpot, glastopf - build errors

--- honeynet/kippo/install.sh
+++ honeynet/kippo/install.sh
@@ -22,14 +22,14 @@ apt-get update -y 2>&1 | tee -a $logfile
 apt-get install git wget python-dev openssl python-openssl python-pyasn1 python-twisted -y 2>&1 | tee -a $logfile
 
 #user
-useradd -d /opt/kippo -s /sbin/nologin -M kippo -U 2>&1 tee -a ${logfile}
+useradd -d /opt/kippo -s /sbin/nologin -M kippo -U 2>&1 | tee -a ${logfile}
 
 #install kippo
 cd /opt/ 2>&1 tee -a ${logfile}
-git clone https://github.com/desaster/kippo.git kippo 2>&1 tee -a ${logfile}
-chown -R kippo:kippo kippo/ 2>&1 tee -a ${logfile}
-cd kippo 2>&1 tee -a ${logfile}
-rm kippo.cfg.dist 2>&1 tee -a ${logfile}
+git clone https://github.com/desaster/kippo.git kippo 2>&1 | tee -a ${logfile}
+chown -R kippo:kippo kippo/ | 2>&1 tee -a ${logfile}
+cd kippo 2>&1 | tee -a ${logfile}
+rm kippo.cfg.dist 2>&1 | tee -a ${logfile}
 
 #Finished
 echo "Finished build correctly - Enjoy!" | tee -a $logfile
